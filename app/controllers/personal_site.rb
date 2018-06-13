08dabcd2afeec2001bcbca5a7af80364b79a61a0
jtrtj <jt.r@live.com>
2018-06-13 10:38:32 -0600

    adds more routes to personal_site

--- app/controllers/personal_site.rb
+++ app/controllers/personal_site.rb
@@ -4,6 +4,7 @@ class PersonalSite
   def self.call(env)
     case env["PATH_INFO"]
     when '/' then index
+    when '/about' then about
     else
       error
     end
@@ -13,7 +14,15 @@ class PersonalSite
     ['200', {'Content-Type' => 'text/html'}, [File.read('./app/views/index.html')]]
   end
 
+  def self.about
+    render_view('about.html')
+  end
+
   def self.error
     ['404', {'Content-Type' => 'text/html'}, [File.read('./app/views/error.html')]]
+  end
+
+  def self.render_view(page, code = '200')
+    [code, {'Content-Type' => 'text/html'}, [File.read("./app/views/#{page}")]]
   end
 end
